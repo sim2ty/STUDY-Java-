db29283d5f26115ebabc3e7ac952bcf7e5015a4c
M14021\student <taeyong.sim23@gmail.com>
2018-07-06 11:48:37 +0900

    랜덤한 두 숫자 나눗셈(나누는 수가 0이면 예외처리)

---
+++ 예외 처리/ExceptionDevide.java
@@ -0,0 +1,30 @@
+﻿package day15;
+public class ExceptionDevide {
+	public static void main(String[] args) {
+		System.out.println("수행시작");
+		try {
+			int num1 = (int)(Math.random()*10);
+			int num2 = (int)(Math.random()*10);
+			System.out.println("num1 : " + num1 + ", num2 : " + num2);
+			int result = num1/num2;
+			System.out.println("연산 결과 : "+result);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("두개의 프로그램 아규먼트를 입력하세요");
+		} catch (ArithmeticException e) {
+			System.out.println("두 번째 숫자는 0일 수 없어요");	
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass().getName());
+			System.out.println(e);
+		} catch (NumberFormatException e) {
+			System.out.println("숫자만 입력하세요");
+		} finally {
+			System.out.println("이 블럭은 반드시 수행합니다.");
+		}
+		System.out.println("수행종료");
+	}
+}
+
+
+
+
+
