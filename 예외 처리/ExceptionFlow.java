69b2fc11815a1c595f1ebefc96f6df51b0decbb8
M14021\student <taeyong.sim23@gmail.com>
2018-07-06 12:42:58 +0900

    예외처리 흐름

---
+++ 예외 처리/ExceptionFlow.java
@@ -0,0 +1,48 @@
+﻿package day15;
+import java.util.Random; 
+class TestException extends Exception {
+	TestException(String message){
+		super(message);
+	}
+}
+public class ExceptionFlow {
+	public static void main(String[] args) {
+		System.out.println("main()수행시작");
+		a();
+		System.out.println("main()수행종료");
+	}
+	static void a() {
+		System.out.println("a()수행시작");
+		try {
+			b();
+		} catch (TestException e){			
+			System.out.println("오류 발생 : "+e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("a()수행종료");
+	}
+	static void b() throws TestException {
+		System.out.println("b()수행시작");
+		c();
+		System.out.println("b()수행종료");
+	}
+	static void c() throws TestException {
+		System.out.println("c()수행시작");
+		boolean flag = new Random().nextBoolean();
+		if(flag){
+			throw new TestException
+			                ("<<:::::테스트로 예외발생시킴:::::>>");
+		}else {
+			System.out.println("ㅋㅋㅋㅋ");
+		}	
+		System.out.println("c()수행종료");
+	}	
+}
+
+
+
+
+
+
+
+
