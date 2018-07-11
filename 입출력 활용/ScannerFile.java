7f04d27eaa278695ea3824124b06913ba719b260
M14021\student <taeyong.sim23@gmail.com>
2018-07-11 17:03:16 +0900

    data.txt 로부터 숫자를 읽어서 합계와 평균을 계산하는 코드

---
+++ 입출력 활용/ScannerFile.java
@@ -0,0 +1,19 @@
+﻿package day18;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ScannerFile {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(new File("data.txt"));
+		int sum = 0;
+		int cnt = 0;
+		while (sc.hasNextInt()) {
+			sum += sc.nextInt();
+			cnt++;
+		}
+		System.out.println("sum="+sum);
+		System.out.printf("average=%.2f", (double)sum/cnt);
+		sc.close();
+	}
+}
