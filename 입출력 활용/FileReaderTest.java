522faa2b0adb91b3cfd6ae5498f08ae80bc87aaf
M14021\student <taeyong.sim23@gmail.com>
2018-07-11 15:34:21 +0900

    FileReader를 사용하여 output.txt 파일의 내용을 읽어들여서 화면에 출력하는 코드

---
+++ 입출력 활용/FileReaderTest.java
@@ -0,0 +1,24 @@
+﻿package day18;
+
+import java.io.*;
+
+public class FileReaderTest {
+	public static void main(String args[]) {
+		try (FileReader reader = new FileReader("c:/iotest/output.txt");){
+			int data;
+			System.out.println(reader.getEncoding());		//encoding 형식을 읽어들인다.
+			while (true) {
+				data = reader.read();		//data를 한글자씩 읽어들인다.
+				if (data == -1)
+					break;               
+				System.out.print((char)data);
+			}
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("파일이 존재하지 않습니다.");
+		} catch (IOException ioe) {
+			System.out.println("파일을 읽을 수 없습니다.");
+		} 
+	}
+}
+
+
