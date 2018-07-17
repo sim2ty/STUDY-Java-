3b2fe8e7ecccba32708d66db4d1a9ca02375e867
sim2ty <sim2ty@naver.com>
2018-07-17 10:21:08 +0900

    Thread에게 Interrupt를 발생시키는 두가지 방법

---
+++ Thread/ThreadInterrupt.java
@@ -0,0 +1,42 @@
+﻿package day20;
+
+public class ThreadInterrupt {
+	public static void main(String[] args)  {
+		Thread thread = new PrintThread2();
+		thread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		
+		thread.interrupt(); // 해당 Thread에게 인터럽트execption을 발생시킨다.
+	}
+}
+
+class PrintThread2 extends Thread {
+	public void run() {	
+		// 방법 1
+		try {
+			while(true) {
+				System.out.println("실행 중");
+				Thread.sleep(10000); 			//이 코드가 없으면 인터럽트를 못받는다. (sleep 상태여야 인터럽트를 받을 수 있다.)
+			}	
+		} catch(InterruptedException e) {	
+			
+		}
+		
+		// 방법 2
+		/*while(true) {
+			System.out.println("실행 중");
+			if(Thread.interrupted()) {			
+				System.out.println("인터럽트!!!");
+				break;
+			}
+		}*/
+		
+		System.out.println("자원 정리");
+		System.out.println("실행 종료");
+	}
+}
+
