5f342c333e90a97e8c18a6fcdeaaef092c65e05a
sim2ty <sim2ty@naver.com>
2018-07-17 10:09:00 +0900

    Thread 이용하여 1부터 1000까지의 합 구하는 코드

---
+++ Thread/Thread_Sum.java
@@ -0,0 +1,32 @@
+﻿package day20;
+
+public class Thread_Sum {
+	public static void main(String[] args) {
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		try {
+			sumThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("1~1000 합: " + sumThread.getSum());
+	}
+}
+
+class SumThread extends Thread {	
+	private long sum;
+	
+	public long getSum() {
+		return sum;
+	}
+
+	public void setSum(long sum) {
+		this.sum = sum;
+	}
+
+	public void run() {
+		for(int i=1; i<=1000; i++) {
+			sum+=i;
+		}
+	}
+}
